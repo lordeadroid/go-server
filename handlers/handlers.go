2945c29834a60903f83c05dfc08b109c537aa037
lordeadroid <rishabhrawat34@gmail.com>
2024-09-16 20:04:35 +0530

    feat: added cors

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -26,27 +26,37 @@ func nextPos(state []string) int {
 	}
 
 	return nextPos(state)
-
 }
 
 func GetNextPosition(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	if req.Method == http.MethodOptions {
+		res.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if req.Method != http.MethodPost {
 		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	body, err := io.ReadAll(req.Body)
-	defer req.Body.Close()
-
-	var state []string
-
-	err = json.Unmarshal(body, &state)
-	pos := nextPos(state)
-
 	if err != nil {
 		http.Error(res, "Error reading request body", http.StatusInternalServerError)
 		return
 	}
+	defer req.Body.Close()
 
+	var state []string
+	err = json.Unmarshal(body, &state)
+	if err != nil {
+		http.Error(res, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	pos := nextPos(state)
 	fmt.Fprintf(res, "%d", pos)
 }
